11e9e7d688a0a0f495e3c9e3ed2eb28b520c8c05
Ubuntu <ubuntu@ip-100-93-40-185.us-west-2.compute.internal>
2019-12-04 06:55:56 +0000

    first commit

---
+++ chapter2/chapter2.py
@@ -0,0 +1,16 @@
+import torchvision
+from torchvision import transforms
+
+train_data_path = "./train/"
+
+transforms = transforms.Compose([
+    transforms.Resize(64),
+    transforms.ToTensor(),
+    transforms.Normalize(mean = [0.485, 0.456, 0.406],
+                        std =[0.229, 0.224, 0.225])
+    ])
+
+train_data = torchvision.datasets.ImageFolder(root=train_data_path, transform=transforms)
+
+val_data_path = "./val/"
+val_data = torchvision.datasets.ImageFolder(root=val_data_path, transform=transforms)
