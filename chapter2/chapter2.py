d178e697f9a3e668820e73592ecfb386b96f6c65
astronomy88 <astronomy88@yahoo.com>
2019-12-04 07:04:33 +0000

    Fleshing out some more data loading

--- chapter2/chapter2.py
+++ chapter2/chapter2.py
@@ -14,3 +14,11 @@ train_data = torchvision.datasets.ImageFolder(root=train_data_path, transform=tr
 
 val_data_path = "./val/"
 val_data = torchvision.datasets.ImageFolder(root=val_data_path, transform=transforms)
+
+test_data_path = "./test/"
+test_data = torchvision.datasets.ImageFolder(root=test_data_path, transform=transforms)
+
+batch_size = 64
+train_data_loader = data.DataLoader(train_data, batch_size=batch_size)
+val_data_loader = data.DataLoader(val_data, batch_size=batch_size)
+test_data_loader = data.DataLoader(test_data, batch_size=batch_size)
